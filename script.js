1b3ad82f2b9dc0d0f70fb79e69a44832be346032
John Doe <cesarmattae@gmail.com>
2024-09-19 14:20:48 +0700

    change text

--- script.js
+++ script.js
@@ -17,71 +17,71 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const weapons = [
-  { name: 'stick', power: 5 },
-  { name: 'dagger', power: 30 },
-  { name: 'claw hammer', power: 50 },
-  { name: 'sword', power: 100 }
+  { name: 'Water', power: 5 },
+  { name: 'lemon tea', power: 30 },
+  { name: 'green tea', power: 50 },
+  { name: 'chrysanthemum tea', power: 100 }
 ];
 const monsters = [
   {
-    name: "slime",
+    name: "Pad Thai",
     level: 2,
     health: 15
   },
   {
-    name: "fanged beast",
+    name: "Tom Yum",
     level: 8,
     health: 60
   },
   {
-    name: "dragon",
+    name: "Pad Krapaw",
     level: 20,
     health: 300
   }
 ]
 const locations = [
   {
-    name: "town square",
-    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    name: "Food Market",
+    "button text": ["Go to the Drinking Corner", "Try different foods ", "Eat Krapaw"],
     "button functions": [goStore, goCave, fightDragon],
     text: "You are in the town square. You see a sign that says \"Store\"."
   },
   {
-    name: "store",
-    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    name: "drinking corner",
+    "button text": ["Buy 10 health (10 baht)", "Buy some drinks (30 baht)", "Go to the food market entrance"],
     "button functions": [buyHealth, buyWeapon, goTown],
-    text: "You enter the store."
+    text: "You visit the drinking corner."
   },
   {
-    name: "cave",
-    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    name: "different foods",
+    "button text": ["Try the Pad Thai", "Try the Tom Yum", "Go to the food market entrance"],
     "button functions": [fightSlime, fightBeast, goTown],
-    text: "You enter the cave. You see some monsters."
+    text: "You enter the food market. You see some dishes."
   },
   {
-    name: "fight",
-    "button text": ["Attack", "Dodge", "Run"],
+    name: "Eat",
+    "button text": ["Eat", "Brace yourself", "Give up"],
     "button functions": [attack, dodge, goTown],
-    text: "You are fighting a monster."
+    text: "You are eating spicy."
   },
   {
-    name: "kill monster",
+    name: "Succeeding to finish spicy dish",
     "button text": ["Go to town square", "Go to town square", "Go to town square"],
     "button functions": [goTown, goTown, goTown],
-    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+    text: 'You finish the spicy dish. Your tongue is burning but your heart content. You gain experience and some baht.'
   },
 
   {
     name: "lose",
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
-    text: "You die. &#x2620;"
+    text: "The food was too spicy. You passed out and die. &#x2620;"
   },
   { 
     name: "win", 
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"], 
     "button functions": [restart, restart, restart], 
-    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;" 
+    text: "You could finish a Pad Krapow! YOU WIN THE GAME! &#x1F389;" 
   },
   {
     name: "easter egg",
@@ -126,7 +126,7 @@ function buyHealth() {
     goldText.innerText = gold;
     healthText.innerText = health;
   } else {
-    text.innerText = "You do not have enough gold to buy health.";
+    text.innerText = "You do not have enough baht to buy health.";
   }
 }
 
@@ -141,11 +141,11 @@ function buyWeapon() {
       inventory.push(newWeapon);
       text.innerText += " In your inventory you have: " + inventory;
     } else {
-      text.innerText = "You do not have enough gold to buy a weapon.";
+      text.innerText = "You do not have enough baht to buy a drink.";
     }
   } else {
-    text.innerText = "You already have the most powerful weapon!";
-    button2.innerText = "Sell weapon for 15 gold";
+    text.innerText = "You already have the most powerful drink!";
+    button2.innerText = "Sell weapon for 15 baht";
     button2.onclick = sellWeapon;
   }
 }
@@ -158,7 +158,7 @@ function sellWeapon() {
     text.innerText = "You sold a " + currentWeapon + ".";
     text.innerText += " In your inventory you have: " + inventory;
   } else {
-    text.innerText = "Don't sell your only weapon!";
+    text.innerText = "Don't sell your only drink!";
   }
 }
 
@@ -186,13 +186,13 @@ function goFight() {
 }
 
 function attack() {
-  text.innerText = "The " + monsters[fighting].name + " attacks.";
-  text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+  text.innerText = "The " + monsters[fighting].name + " burn your tongue.";
+  text.innerText += " You counter it it with your " + weapons[currentWeapon].name + ".";
   health -= getMonsterAttackValue(monsters[fighting].level);
   if (isMonsterHit()) {
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;    
   } else {
-    text.innerText += " You miss.";
+    text.innerText += " The drink is not effective.";
   }
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
@@ -206,7 +206,7 @@ function attack() {
     }
   }
   if (Math.random() <= .1 && inventory.length !== 1) {
-    text.innerText += " Your " + inventory.pop() + " breaks.";
+    text.innerText += " Your " + inventory.pop() + " spill over.";
     currentWeapon--;
   }
 }
@@ -222,7 +222,7 @@ function isMonsterHit() {
 }
 
 function dodge() {
-  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+  text.innerText = "You resist the spiceness from the " + monsters[fighting].name;
 }
 
 function defeatMonster() {
@@ -275,7 +275,7 @@ function pick(guess) {
     text.innerText += numbers[i] + "\n";
   }
   if (numbers.includes(guess)) {
-    text.innerText += "Right! You win 20 gold!";
+    text.innerText += "Right! You win 20 baht!";
     gold += 20;
     goldText.innerText = gold;
   } else {
